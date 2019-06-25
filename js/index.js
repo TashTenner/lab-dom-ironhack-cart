5615c532c44b39f6cc8f6071bf1a96f215a4a0c5
Tash Tenner <tashfonetikas@gmail.com>
2019-06-25 16:56:38 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,52 +1,76 @@
-function deleteItem(event){
-
+function deleteItem(event) {
+  const deleteAction = document.querySelector(".button");
+  const deletedItem = deleteAction.parentNode;
+  deleteAction.parentNode.parentNode.removeChild(deletedItem);
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {}
 
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
+function updatePriceByProduct(productPrice, index) {}
 
 function getTotalPrice() {
+  let result = 0;
+  let arrayPriceProducts = [];
 
+  const products = document.querySelectorAll(".line2");
+  let totalProducts = document.querySelector(".total-of-products");
+
+  //console.log(totalProducts);
+
+  products.forEach(function(product) {
+    // console.log(product);
+    // console.log(parseFloat(product.querySelector(".qty").value));
+    const priceProduct = parseFloat(
+      product.querySelector(".pricePerUnit").innerHTML
+    );
+    const qty = parseFloat(product.querySelector(".qty").value);
+    result = priceProduct * qty;
+    // console.log(result);
+    product.querySelector(".total-price-for-this-product").innerHTML = result;
+    arrayPriceProducts.push(result);
+    //console.log(arrayPriceProducts);
+  });
+
+  let totalResult = 0;
+  totalResult = arrayPriceProducts.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;
+  }, 0);
+  //console.log(totalResult);
+
+  totalProducts.innerHTML = totalResult;
 }
 
-function createQuantityInput(){
+function createQuantityInput() {}
 
+function createDeleteButton() {}
+
+function createQuantityNode() {}
+
+function createItemNode(dataType, itemData) {}
+
+function createNewItemRow(itemName, itemUnitPrice) {
+  itemName = document.querySelector(".new-name").value;
+  let resetValue1 = document.querySelector(".new-name");
+  resetValue1.value = "";
+
+  itemUnitPrice = document.querySelector(".new-price").value;
+  let resetValue2 = document.querySelector(".new-price");
+  resetValue2.value = "";
 }
 
-function createDeleteButton(){
+function createNewItem() {}
 
-}
+window.onload = function() {
+  var calculatePriceButton = document.getElementById("calc-prices-button");
+  var createItemButton = document.getElementById("new-item-create");
+  var deleteButtons = document.getElementsByClassName("btn-delete");
 
-function createQuantityNode(){
+  if (!getTotalPrice) {
+    calculatePriceButton.onclick = getTotalPrice;
+    createItemButton.onclick = createNewItem;
+  }
 
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
 };
